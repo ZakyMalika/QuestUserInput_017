3efa576628f5469a48cb2c347cfc8d8cd2153863
ZakyMalika <zakymalika0987@gmail.com>
2025-10-31 14:37:46 +0700

    membuat file dimens.xml pada folder resource

--- app/src/main/java/com/example/inputpengguna/FormDataDiri.kt
+++ app/src/main/java/com/example/inputpengguna/FormDataDiri.kt
@@ -1,12 +1,16 @@
 package com.example.inputpengguna
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import java.lang.reflect.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier){
 //    variabel" untuk mengingat data
-//    var textNama
+    var textNama by remember { mutableStateOf(value = " ") }
 
 //    variabel" menyimpan
 
