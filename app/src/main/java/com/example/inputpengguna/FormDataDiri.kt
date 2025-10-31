0854a539972fb657f9b8cdae71d827dd153a6311
ZakyMalika <zakymalika0987@gmail.com>
2025-10-31 14:22:07 +0700

    first Commit

---
+++ app/src/main/java/com/example/inputpengguna/FormDataDiri.kt
@@ -0,0 +1,14 @@
+package com.example.inputpengguna
+
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier){
+//    variabel" untuk mengingat data
+//    var textNama
+
+//    variabel" menyimpan
+
+//    val gender:List<String> = listOf("Laki-laki","Perempuan")
+}
