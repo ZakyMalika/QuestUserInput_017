fd88034edf70afa205801c889c5007c0858277ff
ZakyMalika <zakymalika0987@gmail.com>
2025-10-31 14:48:02 +0700

    membuat column dan outlinedtextfield untuk nama

--- app/src/main/java/com/example/inputpengguna/FormDataDiri.kt
+++ app/src/main/java/com/example/inputpengguna/FormDataDiri.kt
@@ -1,10 +1,19 @@
 package com.example.inputpengguna
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import java.lang.reflect.Modifier
 
 @Composable
@@ -20,4 +29,15 @@ fun FormDataDiri(modifier: Modifier){
     var JK by remember { mutableStateOf(value = " ") }
 
     val gender:List<String> = listOf("Laki-laki","Perempuan")
+
+    Column(modifier = Modifier.padding(top=50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(width = 250.dp)
+        )
+    }
 }
